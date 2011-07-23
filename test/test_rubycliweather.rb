0f2750d7dd6f13f6a7e3a0b8ae0548c19a30167e
ip2k <github@seanp2k.endjunk.com>
2011-07-22 20:54:58 -0400

    init

--- test/test_rubycliweather.rb
+++
@@ -1,7 +0,0 @@
-require 'helper'
-
-#class TestRubycliweather < Test::Unit::TestCase
-#  should "forget about tests for now" do
-#    flunk "hey buddy, you should probably rename this file and start testing for real"
-#  end
-#end
