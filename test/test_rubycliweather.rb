6d89fe4e153b47e58cf02ead0e963831ee6e77b2
ip2k <github@seanp2k.endjunk.com>
2011-07-22 20:53:56 -0400

    init

--- test/test_rubycliweather.rb
+++ test/test_rubycliweather.rb
@@ -1,7 +1,7 @@
 require 'helper'
 
-class TestRubycliweather < Test::Unit::TestCase
-  should "probably rename this file and start testing for real" do
-    flunk "hey buddy, you should probably rename this file and start testing for real"
-  end
-end
+#class TestRubycliweather < Test::Unit::TestCase
+#  should "forget about tests for now" do
+#    flunk "hey buddy, you should probably rename this file and start testing for real"
+#  end
+#end
